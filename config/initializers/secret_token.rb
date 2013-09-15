86c00a0b7df86588ff144c0d39bcb8468e32a439
albanlv <albanlv@gmail.com>
2013-09-15 18:16:52 +0200

    merged

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-Discourse::Application.config.secret_token = ENV["SECRET_TOKEN"]
-=======
 # We have had lots of config issues with SECRET_TOKEN to avoid this mess we are moving it to redis
 #  if you feel strongly that it does not belong there use ENV['SECRET_TOKEN']
 #
@@ -12,6 +9,5 @@ unless token
     $redis.set('SECRET_TOKEN',token)
   end
 end
->>>>>>> 982b763216a6109ab5dd108a3e3851b5f183bea9
 
 Discourse::Application.config.secret_token = token
